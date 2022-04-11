d2b63eee8d9169261adcb91e64eaf342bd9e6213
spook-work <hersgori4@protonmail.com>
2022-04-11 21:30:27 +0300

    implement brain games calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,8 @@
+#!/home/spook/.nvm/versions/node/v17.4.0/bin/node
+import brainGamesCalc from '../src/games/calc.js';
+
+const main = () => {
+  brainGamesCalc();
+};
+
+main();
---
+++ src/games/calc.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import runGame from '../index.js';
+import getRandomNumber from '../random.js';
+
+const ops = ['-', '+', '*'];
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @param {string} op
+ * @return {number|null}
+ */
+function evalExpression(num1, num2, op) {
+  switch (op) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return null;
+  }
+}
+
+/**
+ * @return {bool}
+ */
+function isEqualCalc() {
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
+  const op = ops[getRandomNumber(0, 3)];
+  console.log(`Question: ${num1} ${op} ${num2}`);
+
+  const answer = evalExpression(num1, num2, op);
+  const yourAnswer = Number(readlineSync.question('Your answer: '));
+  return answer === yourAnswer;
+}
+
+const gameDescription = 'What is the result of the expression?';
+
+const brainGamesCalc = () => runGame(gameDescription, isEqualCalc);
+
+export default brainGamesCalc;
