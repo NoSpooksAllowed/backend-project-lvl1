7ba14d00fdd360c4062095e393c562c336e30bda
spook-work <hersgori4@protonmail.com>
2022-04-11 15:01:42 +0300

    make linter fixes

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,9 @@
 #!/home/spook/.nvm/versions/node/v17.4.0/bin/node
 import greeting from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-greeting();
+const main = () => {
+  console.log('Welcome to the Brain Games!');
+  greeting();
+};
+
+main();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,12 @@
 import readlineSync from 'readline-sync';
 
 /**
- * @return {void}
+ * @return {string}
  */
 function greeting() {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 }
 
 export default greeting;
