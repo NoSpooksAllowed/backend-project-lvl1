5474fe92ac67989335fb389bad85722ee0672141
spook-work <hersgori4@protonmail.com>
2022-04-11 21:30:56 +0300

    fix getRandimNumber function

--- src/random.js
+++ src/random.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+  return Number(Math.floor(Math.random() * (max - min) + min));
 }
 
 export default getRandomNumber;
