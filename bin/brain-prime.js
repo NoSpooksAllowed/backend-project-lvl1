5ec6eb8e411fbdfa9f4e0cd3e12253eda8fdd650
spook-work <hersgori4@protonmail.com>
2022-04-12 18:53:12 +0300

    make linter fixes

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/home/spook/.nvm/versions/node/v17.4.0/bin/node
-import brainGamesPrime from "../src/games/prime.js";
+import brainGamesPrime from '../src/games/prime.js';
 
 const main = () => {
   brainGamesPrime();
--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,6 @@
-import readlineSync from "readline-sync";
-import runGame from "../index.js";
-import getRandomNumber from "../random.js";
+import readlineSync from 'readline-sync';
+import runGame from '../index.js';
+import getRandomNumber from '../random.js';
 
 /**
  * @return {Array}
@@ -9,25 +9,24 @@ function isPrime() {
   const num = getRandomNumber(1, 100);
   console.log(num);
 
-  const answer = ((num) => {
-    if (num <= 1) {
-      return "no";
+  const answer = ((a) => {
+    if (a <= 1) {
+      return 'no';
     }
-    for (let i = 2; i < num; i++) {
-      if (num % i === 0) {
-        return "no";
+    for (let i = 2; i < a; i += 1) {
+      if (a % i === 0) {
+        return 'no';
       }
     }
 
-    return "yes";
+    return 'yes';
   })(num);
 
-  const yourAnswer = readlineSync.question("Your answer: ");
+  const yourAnswer = readlineSync.question('Your answer: ');
   return [answer, yourAnswer, answer === yourAnswer];
 }
 
-const gameDescription =
-  'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const brainGamesPrime = () => runGame(gameDescription, isPrime);
 
