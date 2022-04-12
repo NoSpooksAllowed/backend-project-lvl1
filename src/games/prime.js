ee65edd440958478943cc20a8a7ef1ebc3b73377
spook-work <hersgori4@protonmail.com>
2022-04-12 18:56:18 +0300

    created findPrimeAnswer function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,24 @@
-import readlineSync from 'readline-sync';
-import runGame from '../index.js';
-import getRandomNumber from '../random.js';
+import readlineSync from "readline-sync";
+import runGame from "../index.js";
+import getRandomNumber from "../random.js";
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+function findPrimeAnswer(num) {
+  if (num <= 1) {
+    return "no";
+  }
+
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return "no";
+    }
+  }
+
+  return "yes";
+}
 
 /**
  * @return {Array}
@@ -9,24 +27,14 @@ function isPrime() {
   const num = getRandomNumber(1, 100);
   console.log(num);
 
-  const answer = ((a) => {
-    if (a <= 1) {
-      return 'no';
-    }
-    for (let i = 2; i < a; i += 1) {
-      if (a % i === 0) {
-        return 'no';
-      }
-    }
+  const answer = findPrimeAnswer(num);
 
-    return 'yes';
-  })(num);
-
-  const yourAnswer = readlineSync.question('Your answer: ');
+  const yourAnswer = readlineSync.question("Your answer: ");
   return [answer, yourAnswer, answer === yourAnswer];
 }
 
-const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const gameDescription =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const brainGamesPrime = () => runGame(gameDescription, isPrime);
 
